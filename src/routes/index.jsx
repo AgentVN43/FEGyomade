1731d3fd64751318454712bc8bdf8bb7aad2edbd
thanhvancu4 <20110722@student.hcmute.edu.vn>
2024-03-19 14:01:17 +0700

    update listProduct; add page DetailProduct

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Product from '../pages/Product';
 import Home from '../pages/Home';
+import DetailProduct from '../pages/DetailProduct';
 
 const Router = () => (
   <Routes>
-    <Route path="/" element={<Home/>} />
+    <Route path="/" element={<Home />} />
     <Route path="/product" element={<Product />} />
+    <Route path="/product_detail" element={<DetailProduct />} />
   </Routes>
 );
 
