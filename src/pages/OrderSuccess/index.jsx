d2230305d11653411ba8da761a6a3e3e40d823b7
Nguyen Kim An <annk.sale@gmail.com>
2024-03-21 11:46:29 +0700

    test deploy

--- src/pages/OrderSuccess/index.jsx
+++ src/pages/OrderSuccess/index.jsx
@@ -7,6 +7,7 @@ export default function OrderSuccess() {
         <>
             <HeadingCart title={"Order Success"} currentPage={"Success"} />
             <MainOrderSuccess />
+            <p>test</p>
         </>
     )
 }
