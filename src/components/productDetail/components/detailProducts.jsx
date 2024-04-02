dfe710164958de9082889d4583a98df4cf19075a
Nguyen Kim An <annk.sale@gmail.com>
2024-04-02 22:47:47 +0700

    Update load price follow color

--- src/components/productDetail/components/detailProducts.jsx
+++ src/components/productDetail/components/detailProducts.jsx
@@ -14,6 +14,7 @@ export default function DetailProduct({ onAddToCart }) {
   const [countItem, setcountItem] = useState(0);
   const [hoveredSize, setHoveredSize] = useState(null);
   const [selectedColor, setSelectedColor] = useState(null);
+  const [filteredData, setFilteredData] = useState([]);
 
   const { addToCart } = useContext(CartContext);
   const { incrementQuantity, decrementQuantity, quantity } = useCart();
@@ -44,17 +45,15 @@ export default function DetailProduct({ onAddToCart }) {
 
   const name = productDetail.name;
   const inventory = productDetail.inventory;
-  const price = [...new Set(productVariants.map((item) => item.price))];
-  const color = [...new Set(productVariants.map((item) => item.color))];
-  const hex_code = [...new Set(productVariants.map((item) => item.hex_code))];
+  const price = [...new Set(filteredData.flat().map((item) => item.price))];
+  // const color = [...new Set(productVariants.map((item) => item.color))];
+  // const hex_code = [...new Set(productVariants.map((item) => item.hex_code))];
 
-  const isSizeDisabled = (selectedColor) => {
-    return productVariants.some(
-      (item) => item[0].color === selectedColor && item.remain_quantity <= 0
-    );
-  };
-
-  const [filteredData, setFilteredData] = useState([]);
+  // const isSizeDisabled = (selectedColor) => {
+  //   return productVariants.some(
+  //     (item) => item[0].color === selectedColor && item.remain_quantity <= 0
+  //   );
+  // };
 
   const handleColorSelect = (color) => {
     setSelectedColor(color);
@@ -68,15 +67,15 @@ export default function DetailProduct({ onAddToCart }) {
   };
 
   console.log(filteredData);
+  console.log(price);
+  // const handleSizeSelect = (size) => {
+  //   setSelectedSize(size);
+  // };
 
-  const handleSizeSelect = (size) => {
-    setSelectedSize(size);
-  };
-  const stringColor = color[0];
+  // const stringColor = color[0];
 
   const clickSize = (size) => {
     setSelectedSize((prevSize) => (prevSize === size ? null : size));
-    // setQuantity(1);
     setErrorMessage("");
   };
 
@@ -150,128 +149,9 @@ export default function DetailProduct({ onAddToCart }) {
       </p> */}
     </Tooltip>
   );
-
+  // {price.toLocaleString()}
   return (
     <>
-      {/* <div className="cs_single_product_details">
-        <h2 className="cs_fs_37 cs_semibold">{name}</h2>
-        <div className="cs_single_product_review">
-          <div className="cs_rating_container">
-            <div className="cs_rating cs_size_sm" data-rating={5}>
-              <div className="cs_rating_percentage" />
-            </div>
-          </div>
-          <span>(5)</span>
-          <span>
-            Stock: <span className="cs_accent_color">{inventory}</span>
-          </span>
-        </div>
-        <h4 className="cs_single_product_price cs_fs_21 cs_primary_color cs_semibold">
-          {price.toLocaleString()}
-        </h4>
-        <hr />
-        <div className="cs_single_product_details_text">
-          <ul className="mb-0">
-            <li>
-              Hình ảnh sản phẩm là ảnh thật do shop tự chụp và giữ bản quyền
-              hình ảnh.
-            </li>
-            <li>Đảm bảo vải chất lượng.</li>
-            <li>
-              Sản phẩm được kiểm tra kĩ càng, cẩn thận và tư vấn nhiệt tình
-              trước khi gói hàng giao cho Quý Khách
-            </li>
-            <li>
-              Quý Khách Hàng có sẵn, giao hàng ngay khi nhận được đơn Giao hàng
-              trên toàn quốc, nhận hàng trả tiền
-            </li>
-          </ul>
-        </div>
-        <div className="cs_single_product_size">
-          <h4 className="cs_fs_16 cs_medium">Size:</h4>
-          <ul
-            className="cs_size_filter_list cs_mp0"
-            onMouseLeave={handleMouseLeave}
-          >
-            {productVariants.map((item, index) => (
-              <li key={index} onMouseEnter={() => handleMouseEnter(item.size)}>
-                <input
-                  type="radio"
-                  name="size"
-                  disabled={isSizeDisabled(item.size)}
-                  checked={selectedSize === item.size}
-                  onChange={() => clickSize(item.size)}
-                />
-                <span
-                  className={isSizeDisabled(item.size) ? "disabled" : ""}
-                  style={{
-                    ...(selectedSize === item.size && {
-                      borderColor: "#fc5f49",
-                      backgroundColor: "#fc5f49",
-                      color: "#fff",
-                    }),
-                  }}
-                >
-                  {item.size}
-                </span>{" "}
-              </li>
-            ))}
-          </ul>
-          {hoveredSize && (
-            <p>
-              Size {hoveredSize} còn:{" "}
-              {
-                productVariants.find((item) => item.size === hoveredSize)
-                  ?.remain_quantity
-              }{" "}
-              cái
-            </p>
-          )}
-          {errorMessage && <span>{errorMessage}</span>}
-          <SizeTutor />
-        </div>
-        <div className="cs_single_product_color ">
-          <h4 className="cs_fs_16 cs_medium">Màu:</h4>
-          <ul className="cs_color_filter_list cs_type_1 cs_mp0">
-            {color.map((item, index) => (
-              <li key={index}>
-                <div className="cs_color_filter">
-                  <input type="checkbox" name="color" />
-                  <span
-                    className="cs_color_filter_circle cs_accent_bg"
-                    style={{ backgroundColor: hex_code }}
-                  />
-                  <span className="cs_color_text">{item}</span>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="cs_action_btns">
-          <div className="cs_quantity">
-            <button
-              className="cs_quantity_btn cs_increment"
-              onClick={() => incrementQuantity()}
-            >
-              <i className="fa-solid fa-angle-up" />
-            </button>
-            <span className="cs_quantity_input">{quantity}</span>
-            <button
-              className="cs_quantity_btn cs_decrement"
-              onClick={() => decrementQuantity()}
-            >
-              <i className="fa-solid fa-angle-down" />
-            </button>
-          </div>
-
-          <button
-            className="cs_btn cs_style_1 cs_fs_16 cs_medium"
-            onClick={handleAddToCart}
-          >
-            Add to Cart
-          </button>
-        </div>
-      </div> */}
       <div className="cs_single_product_details">
         <h2 className="cs_fs_37 cs_semibold">{name}</h2>
         <div className="cs_single_product_review">
@@ -286,7 +166,7 @@ export default function DetailProduct({ onAddToCart }) {
           </span>
         </div>
         <h4 className="cs_single_product_price cs_fs_21 cs_primary_color cs_semibold">
-          {price.toLocaleString()}
+          {price.map((item) => item.toLocaleString())}
         </h4>
         <hr />
         <div className="cs_single_product_details_text">
@@ -301,8 +181,11 @@ export default function DetailProduct({ onAddToCart }) {
               trước khi gói hàng giao cho Quý Khách
             </li>
             <li>
-              Quý Khách Hàng có sẵn, giao hàng ngay khi nhận được đơn Giao hàng
-              trên toàn quốc, nhận hàng trả tiền
+              Hàng có sẵn, giao hàng ngay khi nhận được đơn Giao hàng
+              trên toàn quốc, nhận hàng, đồng kiểm và thanh toán.
+            </li>
+            <li>
+              Những sản phẩm có nhiều hơn 1 màu, vui lòng chọn màu trước để hiển thị giá
             </li>
           </ul>
         </div>
@@ -354,12 +237,6 @@ export default function DetailProduct({ onAddToCart }) {
                     key={item.id}
                     onMouseEnter={() => handleMouseEnter(item.size)}
                   >
-                    {/* <button
-                      disabled={item.remain_quantity <= 0}
-                      onClick={() => clickSize(item.size)}
-                    >
-                      {item.size}
-                    </button> */}
                     <input
                       type="radio"
                       name="size"
