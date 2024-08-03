503addb6244fac8921d76aa3946db5faff303f27
thanhvancu4 <20110722@student.hcmute.edu.vn>
2024-08-03 09:30:01 +0700

    fix height item slider

--- src/components/featuredItem/components/sliderproduct.jsx
+++ src/components/featuredItem/components/sliderproduct.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import Product from "../../product";
+import './sliderProduct.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export default function SlideProduct({categoryId}) {
+export default function SlideProduct({ categoryId }) {
   const [productCategory, setproductCategory] = useState([]);
-  
+
   useEffect(() => {
     fetch(`https://gyomade.vn/mvc/products/category/${categoryId}`)
       .then((response) => response.json())
@@ -22,7 +23,7 @@ export default function SlideProduct({categoryId}) {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 1000,
     rows: 2,
     slidesPerRow: 1,
@@ -54,39 +55,39 @@ export default function SlideProduct({categoryId}) {
 
   return (
     <>
-      <div className="container-fluid">
-        <div className="cs_tabs">
-          <div className="cs_tab active" id="tab_tshirt">
-            <div className="cs_slider position-relative cs_hover_arrow">
-              <div className="cs_slider_container">
-                <div className="cs_slider_wrapper">
-                  <div className="slick_slide_in">
-                    <Slider {...settings}>
-                      {productCategory.map((product, index) => (
-                        <div key={index}>
-                          <Product
-                            key={index}
-                            id={product.id}
-                            imageUrl={product.image}
-                            price={product.price}
-                            name={product.name}
-                            slug={product.slug}
-                            inventory={product.inventory}
-                            display_id={product.display_id}
-                            product_id={product.id}
-                            color={product.color}
-                            size={product.size}
-                          />
-                        </div>
-                      ))}
-                    </Slider>
+      {/* <div className="container-fluid"> */}
+      {/* <div className="cs_tabs"> */}
+      {/* <div className="cs_tab active" id="tab_tshirt"> */}
+      <div className="cs_slider position-relative cs_hover_arrow">
+        <div className="cs_slider_container">
+          <div className="cs_slider_wrapper">
+            <div className="slick_slide_in" style={{ height: '100%' }}>
+              <Slider {...settings} className="carousel-slide">
+                {productCategory.map((product, index) => (
+                  <div key={index}>
+                    <Product
+                      key={index}
+                      id={product.id}
+                      imageUrl={product.image}
+                      price={product.price}
+                      name={product.name}
+                      slug={product.slug}
+                      inventory={product.inventory}
+                      display_id={product.display_id}
+                      product_id={product.id}
+                      color={product.color}
+                      size={product.size}
+                    />
                   </div>
-                </div>
-              </div>
+                ))}
+              </Slider>
             </div>
           </div>
         </div>
       </div>
+      {/* </div> */}
+      {/* </div> */}
+      {/* </div> */}
     </>
   );
 }
